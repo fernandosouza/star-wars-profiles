a099ad934bf5b188045ca27900c5dda700e24eba
Fernando Souza <fernando.souza@circula.com>
2021-11-14 18:59:20 +0100

    testing custom worker

--- worker/index.ts
+++ worker/index.ts
@@ -1,6 +1,6 @@
 import { util } from './util';
 
-declare let self
+declare let self;
 
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
@@ -9,8 +9,15 @@ declare let self
 
 util();
 
+self.addEventListener('fetch', (event) => {
+  // This can implement request cache strategy.
+  event.respondWith(fetch(event.request));
+  const { method, url } = event.request;
+  console.log(`Fetch ${method}:${url} is happening`);
+});
+
 // listen to message event from window
-self.addEventListener('message', event => {
+self.addEventListener('message', (event) => {
   // HOW TO TEST THIS?
   // Run this in your browser console:
   //     window.navigator.serviceWorker.controller.postMessage({command: 'log', message: 'hello world'})
@@ -19,30 +26,32 @@ self.addEventListener('message', event => {
   console.log(event?.data);
 });
 
-self.addEventListener('push',  (event) => {
-  const data = JSON.parse(event?.data.text() || '{}')
+self.addEventListener('push', (event) => {
+  const data = JSON.parse(event?.data.text() || '{}');
   event?.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.message,
-      icon: '/icons/android-chrome-192x192.png'
+      icon: '/icons/android-chrome-192x192.png',
     })
-  )
-})
+  );
+});
 
-self.addEventListener('notificationclick',  (event) => {
-  event?.notification.close()
+self.addEventListener('notificationclick', (event) => {
+  event?.notification.close();
   event?.waitUntil(
-    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
-      if (clientList.length > 0) {
-        let client = clientList[0]
-        for (let i = 0; i < clientList.length; i++) {
-          if (clientList[i].focused) {
-            client = clientList[i]
+    self.clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then(function (clientList) {
+        if (clientList.length > 0) {
+          let client = clientList[0];
+          for (let i = 0; i < clientList.length; i++) {
+            if (clientList[i].focused) {
+              client = clientList[i];
+            }
           }
+          return client.focus();
         }
-        return client.focus()
-      }
-      return self.clients.openWindow('/')
-    })
-  )
-})
+        return self.clients.openWindow('/');
+      })
+  );
+});
